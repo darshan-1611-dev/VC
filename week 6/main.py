596ee5e9326a1de96e57655bfb2261e8c91c30c9
Darshan Dhanani <darshandhanani1611@gmail.com>
2024-05-21 16:54:10 +0300

    upload week 6

--- week 6/main.py
+++ week 6/main.py
@@ -1,41 +1,47 @@
-import dask.dataframe as dd
 import time
-import os
+import yaml
+import dask.dataframe as dd
 
 __author__ = 'Darshan Dhanani'
 
-FILE_NAME = "D:/jainam/nifty_data/result/result/option/BANKNIFTY_OPTION.csv"
-
 start_time = time.time()
 
-def read_file_metadata(filepath):
-    #dictionary
-    firstDict = {}
-        
-    #main library that holds stats
-    stats = os.stat(filepath)
-        
-    attrs = {
-        'File Name': stats.st_name,
-        'Size (KB)': sizeFormat(stats.st_size),
-        'Creation Date': timeConvert(stats.st_ctime),
-        'Modified Date': timeConvert(stats.st_mtime),
-        'Last Access Date': timeConvert(stats.st_atime),
-        }
-  
-    firstDict[name] = attrs 
+def column_name_modification(df):
+    df.columns.tolist()
 
+    new_column_list = []
+
+    for string  in df.columns.tolist():
+        new_column_list.append(''.join(e for e in string if e.isalnum()).lower())
+
+    df.columns = new_column_list
     
-    return firstDict 
+    return df
+
+
+def column_name_validation(df_columns, config_data_columns):
+    if len(df_columns) == len(config_data_columns) and list(config_data_columns)  == list(df_columns):
+        return 1
+    else:
+        return 0
+
+# Read YAML file
+with open("file.yaml", 'r') as stream:
+    data_loaded = yaml.safe_load(stream)
     
-# read_file_metadata(FILE_NAME) 
-# exit()
-df = dd.read_csv(FILE_NAME, header=None)  # dask 
+    file_name = data_loaded['file_name'] + '.' + data_loaded['file_type']
+    config_data_columns = data_loaded['columns']
+    
+df = dd.read_csv(file_name)  # read file with dask 
 
-print(df.head(2))
-
-# print(len(dataframe))
-
+df = column_name_modification(df)  # change column name 
+    
+# validation of column name 
+if column_name_validation(df.columns.tolist(), config_data_columns):  
+    print("Columns Validation passed")
+else:
+    print("Columns Validation failed")
+    
 execution_time = time.time() - start_time
 print("---------------------------------------------")
 print(f"Execution Time: {execution_time:.4f} seconds")
