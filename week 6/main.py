8a82c22a7e4a7d1efaf9554a8213448af372874a
Darshan Dhanani <darshandhanani1611@gmail.com>
2024-05-21 12:22:50 +0300

    upload week 6

--- week 6/main.py
+++ week 6/main.py
@@ -8,13 +8,34 @@ FILE_NAME = "D:/jainam/nifty_data/result/result/option/BANKNIFTY_OPTION.csv"
 
 start_time = time.time()
 
-# dataframe = dd.read_csv(FILE_NAME)  # dask
+def read_file_metadata(filepath):
+    #dictionary
+    firstDict = {}
+        
+    #main library that holds stats
+    stats = os.stat(filepath)
+        
+    attrs = {
+        'File Name': stats.st_name,
+        'Size (KB)': sizeFormat(stats.st_size),
+        'Creation Date': timeConvert(stats.st_ctime),
+        'Modified Date': timeConvert(stats.st_mtime),
+        'Last Access Date': timeConvert(stats.st_atime),
+        }
+  
+    firstDict[name] = attrs 
 
+    
+    return firstDict 
+    
+# read_file_metadata(FILE_NAME) 
+# exit()
+df = dd.read_csv(FILE_NAME, header=None)  # dask 
 
-print(len(dataframe))
+print(df.head(2))
+
+# print(len(dataframe))
 
 execution_time = time.time() - start_time
 print("---------------------------------------------")
 print(f"Execution Time: {execution_time:.4f} seconds")
-
-
